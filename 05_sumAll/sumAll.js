49c5d8148550965694bf0cb1604f8e1de5dc01db
philip-hargreaves <137929060+philip-hargreaves@users.noreply.github.com>
2024-12-30 18:47:13 +0000

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    result = 0;
 
+    if (!(Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0)) {
+        return "ERROR"
+    }
+
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result
 };
 
 // Do not edit below this line
